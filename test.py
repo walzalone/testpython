d5dde8b0105aff788d628d78381a045053d16ce1
RainWhileLoop <rainwhileloop@gmail.com>
2019-06-03 15:28:10 +0700

    fixed condition 1

--- test.py
+++ test.py
@@ -3,9 +3,9 @@ print('Hello World')
 x = input("value = ")
 
 if x == 1:
-    print ("OK")
+    print ("val = 1")
 else:
-    print ("not ok")
+    print ("val != 1")
 
 print("End")
 print('WTF WORLD DUDEEEEEEEEEEEEEEEEEEEEEEEEEE')
